a8e42176791234c28a7dd2e5f33bb4b3243c2414
Najda Villefranque <najda.villefranque@cnrs.fr>
2023-11-29 14:40:22 +0000

    add copyright and license in py scripts

--- scripts_convert/prep_nam_from_FFCOM_DEF_to_MNHLES.py
+++ scripts_convert/prep_nam_from_FFCOM_DEF_to_MNHLES.py
@@ -9,6 +9,22 @@ A lancer par python3  prep_nam_from_FFCOM_DEF_to_MNH1D.py  CAS SOUS_CAS
 Modification
    2023/11/27 pour nettoyer
 """
+# Copyright (C) 2023 Météo-France
+# Copyright (C) 2023 Centre National de la Recherche Scientifique
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 #Modification a apporter
 # cas avec Rayonnement ex ARMCVP => choix des namelists adaptées
 # voir avec Benoit si on peut tout faire avec Lima en utilisant des setup qui convergent vers ICE3 ou demande trop de temps de calcul?
