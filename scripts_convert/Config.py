GdR-DEPHY/DEPHY-MNH

commit c1c153b0ecf7b2027625ab9fa7d348358ef09a2c

fix str ECRA and activ budget SCM

scripts_convert/Config.py
@@ -499,7 +499,7 @@ class Config:
       rel_times=rel_times_f; list_=list_f;
       if var is not None :
         # not the right dim, or constant forcing => take only the first value
-        if (len(var) != nrf and len(var) != ntf) or (var.min()==var.max()): var = [var[0]]*ntf
+        if (len(var) != nrf and len(var) != nrt) or (var.min()==var.max()): var = [var[0]]*ntf
         if key == "XZ0":
             self.modify("NAM_IDEAL_FLUX", "CUSTARTYPE", "'Z0'")
             self.modify("NAM_IDEAL_FLUX", "XZ0", "%.3f"%(forc(var)[0]))
@@ -579,8 +579,8 @@ class Config:
 
   def deactivate_radiation(self):
     self.modify("NAM_PARAMn", "CRAD", "'NONE'")
-  def activate_radiation(self, rad='ECRA'):
-    self.modify("NAM_PARAMn", "CRAD", "%s"%rad)
+  def activate_radiation(self, rad="'ECRA'"):
+    self.modify("NAM_PARAMn", "CRAD", rad if "'" in rad else "'%s'"%rad)
 
   def deactivate_edkf(self):
     self.modify("NAM_PARAMn", "CSCONV", "'NONE'")

scripts_convert/mesonh.py
@@ -112,8 +112,7 @@ exseg_SCM = {
                   "LLES_CS_MASK"        : ".FALSE.",
                 },
   "NAM_CONDSAMP": { "LCONDSAMP"    : ".FALSE."    },
-  "NAM_BUDGET"  : { "CBUTYPE"      : "'NONE'",
-                    "NBUIH"        : "%i"%SCM_Nhoriz,
+  "NAM_BUDGET"  : { "NBUIH"        : "%i"%SCM_Nhoriz,
                     "NBUJH"        : "%i"%SCM_Nhoriz, 
                     "NBUKH"        : "%i"%SCM_Nverti},
 }

commit e2019dd56ae61327d63d91dddc57e84dfc62a674

fix bug in forcings, introduced in c1c153b

scripts_convert/Config.py
@@ -507,7 +507,10 @@ class Config:
       rel_times=rel_times_f; list_=list_f;
       if var is not None :
         # not the right dim, or constant forcing => take only the first value
-        if (len(var) != nrf and len(var) != nrt) or (var.min()==var.max()): var = [var[0]]*ntf
+        # ntf, ntt = total forcing times (the whole simulation)
+        # nrf, nrt = only for this segment
+        if (len(var) != ntf and len(var) != ntt) or (var.min()==var.max()): 
+            var = [var[0]]*ntf
         if key == "XZ0":
             self.modify("NAM_IDEAL_FLUX", "CUSTARTYPE", "'Z0'")
             self.modify("NAM_IDEAL_FLUX", "XZ0", "%.3f"%(forc(var)[0]))